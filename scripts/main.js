d68087ef22d22bfe54a92d40738dca417a73ec5b
Gabriel Maia <140010302+gabrielmaia7g@users.noreply.github.com>
2024-09-13 18:37:12 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const dropDownMobile = document.querySelector('.drop-down-mobile');
+const btnMobile = document.querySelector('.botaoMobile');
+
+
+btnMobile.addEventListener('click', function() {
+    dropDownMobile.classList.toggle('menu-ativo')
+});
+
+/*const botaoMobile = document.getElementById('botaoMobile');
+
+
+// Adiciona um ouvinte de evento ao botão
+botaoMobile.addEventListener('click', function() {
+    // Alterna a classe 'menu-ativo' para mostrar ou esconder o menu
+    dropDownMenu.classList.toggle('menu-ativo');
+});*/
