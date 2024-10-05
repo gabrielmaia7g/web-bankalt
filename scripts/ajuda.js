382766941de9a1c63920ff80575f994a87efb456
Gabriel Augusto Pereira Maia <gabrielmaia7g@gmail.com>
2024-10-05 18:55:12 -0300

    formatando e validando cpf

---
+++ scripts/ajuda.js
@@ -0,0 +1,10 @@
+const formAjuda = document.querySelector('#formAjuda');
+const btnEnviarMensagem = document.querySelector('#enviarMensagem');
+
+formAjuda.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+})
+
+btnEnviarMensagem.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+})
