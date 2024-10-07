8bd289cbdaa3a4e49440f6faa728b6a068643dd6
Gabriel Augusto Pereira Maia <gabrielmaia7g@gmail.com>
2024-10-06 22:35:11 -0300

    corrigindo nav mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+const btnMobileLista = document.querySelector('.btnMenu')
+const navMobileLista = document.querySelector('.drop-down-mobile');
+
+btnMobileLista.addEventListener('click', ()=> {
+    navMobileLista.classList.toggle('btnMenu-hidden');
+})
 
 
 /*
